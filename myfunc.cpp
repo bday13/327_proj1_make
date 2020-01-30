04566fc271e7db6f74b0ae0fb4e0d2215e2c406b
Brandon Day <brandon.day.17@cnu.edu>
2020-01-29 19:04:20 -0800

    done

---
+++ myfunc.cpp
@@ -0,0 +1,14 @@
+/*
+ * myfunc.cpp
+ *
+ *  Created on: Jan 29, 2020
+ *      Author: bday13
+ */
+#include "myfunc.h"
+
+std::string func()
+{
+	return "hello";
+}
+
+
---
+++ myfunc.h
@@ -0,0 +1,16 @@
+/*
+ * myfunc.h
+ *
+ *  Created on: Jan 29, 2020
+ *      Author: bday13
+ */
+
+//#ifndef MYFUNC_H_
+//#define MYFUNC_H_
+
+#include <iostream>
+std::string func();
+
+
+
+//#endif /* MYFUNC_H_ */
